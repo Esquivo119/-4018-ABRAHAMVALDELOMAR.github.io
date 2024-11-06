2ebb46efd6240ba69205e6f6cc34a06dbc9d0d62
Esquivo119 <luquejuan560@gmail.com>
2024-11-06 09:17:16 -0500

    Add files via upload

---
+++ scriptCelu.js
@@ -0,0 +1,4 @@
+// Mostrar una alerta de bienvenida en la versión móvil
+window.addEventListener('load', () => {
+    alert('Bienvenido a la versión móvil del sitio del colegio I.E. № 4018 Abraham Valdelomar');
+});
